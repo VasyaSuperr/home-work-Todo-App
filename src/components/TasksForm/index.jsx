7fa62d03c9d908ad4b32921d1e652ddde4dde96a
VasyaSuperr <yenakii.vasyl@gmail.com>
2024-08-15 15:03:51 +0300

    Added form for add tasks without realisation add

--- src/components/TasksForm/index.jsx
+++ src/components/TasksForm/index.jsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import { ErrorMessage, Field, Form, Formik } from 'formik';
 
 function TasksForm () {
-  return <div>TasksForm</div>;
+  const initialValues = {
+    task: '',
+  };
+
+  const submitHandler = (value, { resetForm }) => {
+    console.log(value);
+    resetForm();
+  };
+
+  return (
+    <section>
+      <h2>TasksForm</h2>
+      <Formik initialValues={initialValues} onSubmit={submitHandler}>
+        <Form>
+          <Field
+            type='text'
+            name='task'
+            placeholder='Create a new todo...'
+            autoFocus
+          />
+          <ErrorMessage name='task' component='div' />
+        </Form>
+      </Formik>
+    </section>
+  );
 }
 
 export default TasksForm;
---
+++ src/utils/validationSchemas.js
@@ -0,0 +1,3 @@
+import * as yup from 'yup';
+
+export const TASKS_VALIDATION_SCHEMA = yup.object({});
